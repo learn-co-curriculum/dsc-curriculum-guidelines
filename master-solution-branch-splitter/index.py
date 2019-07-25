950e84343165ce015d83219476a901ca1f419b14
Alex Griffith <alexalexgriffith@gmail.com>
2019-07-25 18:58:13 -0400

    Use commit message, push to curriculum

--- master-solution-branch-splitter/index.py
+++ master-solution-branch-splitter/index.py
@@ -10,33 +10,42 @@ MASTER_BRANCH = "master"
 SOLUTION_BRANCH = "solution"
 
 # FUNCTIONS
+
+
 def get_notebook_json(filename="index.ipynb"):
     with open(filename, 'r') as f:
-        data= json.load(f)
+        data = json.load(f)
     return data
+
 
 def is_markdown_cell(cell):
     return cell["cell_type"] == "markdown"
 
+
 def contains_tag(line):
     return SOLUTION_TAG in line.strip().split(" ")
 
+
 def is_solution_cell(cell):
-    if cell["cell_type"] != "code": return False
+    if cell["cell_type"] != "code":
+        return False
 
     # does any line of the cell have the SOLUTION tag anywhere in it
     found_tag = [True for line in cell["source"] if contains_tag(line)]
 
     return bool(len(found_tag))
 
+
 # removes __SOLUTON__ line from tagged code cells
 def untag(cell):
-    if cell["cell_type"] != "code": return cell
+    if cell["cell_type"] != "code":
+        return cell
 
     source = [line for line in cell["source"] if not contains_tag(line)]
 
     cell.update({"source": source})
     return cell
+
 
 def create_master_notebook(nb):
     cells = [
@@ -46,11 +55,14 @@ def create_master_notebook(nb):
     nb.update({"cells": cells})
     return nb
 
+
 def for_master(cell):
     return is_markdown_cell(cell) or not is_solution_cell(cell)
 
+
 def for_sol(cell):
     return is_markdown_cell(cell) or is_solution_cell(cell)
+
 
 def create_sol_notebook(nb):
     cells = [
@@ -60,7 +72,13 @@ def create_sol_notebook(nb):
     nb.update({"cells": cells})
     return nb
 
-def sync_branch(repo, branch, notebook):
+
+def notebook_to_markdown():
+    subprocess.call(["jupyter", "nbconvert", "index.ipynb",  "--to", "markdown"])
+    subprocess.call(["mv", "index.md", "README.md"])
+
+
+def sync_branch(repo, branch, notebook, msg="Curriculum Auto-Sync"):
     # switch to branch if exists or create new branch
     try:
         repo.git.checkout(branch)
@@ -73,20 +91,18 @@ def sync_branch(repo, branch, notebook):
     f.close()
 
     # generate markdown
-    subprocess.call(["jupyter", "nbconvert", "index.ipynb",  "--to", "markdown"])
-    subprocess.call(["mv", "index.md", "README.md"])
+    notebook_to_markdown()
 
     # add, commit, push
     repo.git.add(".")
     try:
-        repo.git.commit("-m", "Master-Solution Auto Sync")
+        repo.git.commit("-m", msg)
         print(f"Added Commit: {repo.commit()}")
     except GitCommandError:
         print("Nothing to commit")
 
     print(f"pushing to remote {branch} branch")
     repo.git.push("origin", branch)
-
 
 
 # RUN
@@ -100,20 +116,26 @@ Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd)
 repo = Repo(os.getcwd())
 git = repo.git
 
-# should raise if there are local unstaged changes
-git.push("origin", CURRICULUM_BRANCH)
 
 try:
     git.checkout(CURRICULUM_BRANCH)
 except GitCommandError:
     raise Exception(f"A branch called {CURRICULUM_BRANCH} must exist")
 
+commit_message = repo.head.commit.message
+notebook_to_markdown()
+repo.git.add(".")
+repo.git.commit("-m", commit_message)
+# should raise if there are local unstaged changes
+print(f"pushing to remote {CURRICULUM_BRANCH} branch")
+git.push("origin", CURRICULUM_BRANCH)
+
 notebook_json   = get_notebook_json()
 master_notebook = create_master_notebook(dict(notebook_json))
 sol_notebook    = create_sol_notebook(dict(notebook_json))
 
-sync_branch(repo, MASTER_BRANCH, master_notebook)
-sync_branch(repo, SOLUTION_BRANCH, sol_notebook)
+sync_branch(repo, MASTER_BRANCH, master_notebook, msg=commit_message)
+sync_branch(repo, SOLUTION_BRANCH, sol_notebook, msg=commit_message)
 
 # clean up
 git.checkout(MASTER_BRANCH)
