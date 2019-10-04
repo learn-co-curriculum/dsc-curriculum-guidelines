20097998ac456f5a65ac3131a85284db327cd09c
Alex Griffith <alexalexgriffith@gmail.com>
2019-10-04 14:12:38 -0400

    Update package_check.py

--- updating_the_conda_env/package_check.py
+++ updating_the_conda_env/package_check.py
@@ -1,13 +1,7 @@
 import cvxpy
-import imblearn
-import pydotplus
+import scs
 import fbprophet
-import folium
 import surprise
-import pandasql
-import xgboost
-import tqdm
-import pyspark
 import nltk
 
 nltk.download('averaged_perceptron_tagger')
